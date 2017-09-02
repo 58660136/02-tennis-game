00ffcd62354e65183d9355598eff0c9127c28c4c
Jame <58660136@go.buu.ac.th>
2017-09-02 15:45:32 +0700

    Fifteen Love when player[A] get first score and Love Fifteen when player[B] get first score

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,9 +1,14 @@
 function TennisGame(){
+  const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+  this.playerAScore = 0
   this.reset = () => {
-
+    this.playerAScore = 0
   }
   this.echo = () =>{
-    return 'Love - Love'
+    return `${scoreString[this.playerAScore]} - Love`
+  }
+  this.playAGetScore = () =>{
+    this.playerAScore++
   }
 }
 test('Echo "Love - Love" when press reset button',() => {
@@ -17,5 +22,17 @@ test('Echo "Love - Love" when press reset button',() => {
   // Assert
   expect(result).toBe('Love - Love')
 
+})
+
+test('Echo "Fifteen - Love" when press button[A] get first score',() => {
+  // Arrange
+  let app = new TennisGame()
+  app.reset()
+  app.playAGetScore();
+  // Act
+  let result = app.echo()
+
+  // Assert
+  expect(result).toBe('Fifteen - Love')
 
 })
