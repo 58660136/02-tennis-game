849399e3168d42c2464ac865c00b128640808b04
Jame <58660136@go.buu.ac.th>
2017-09-02 15:57:16 +0700

    Thirty Forthy when playerA get 2 score and playerB get 3 score

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,14 +1,19 @@
 function TennisGame(){
   const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
   this.playerAScore = 0
+  this.playerBScore = 0
   this.reset = () => {
     this.playerAScore = 0
+    this.playerBScore = 0
   }
   this.echo = () =>{
-    return `${scoreString[this.playerAScore]} - Love`
+    return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
   }
   this.playAGetScore = () =>{
     this.playerAScore++
+  }
+  this.playBGetScore = () =>{
+    this.playerBScore++
   }
 }
 test('Echo "Love - Love" when press reset button',() => {
@@ -24,7 +29,7 @@ test('Echo "Love - Love" when press reset button',() => {
 
 })
 
-test('Echo "Fifteen - Love" when press button[A] get first score',() => {
+test('Echo "Fifteen - Love" when player button[A] get first score',() => {
   // Arrange
   let app = new TennisGame()
   app.reset()
@@ -34,5 +39,35 @@ test('Echo "Fifteen - Love" when press button[A] get first score',() => {
 
   // Assert
   expect(result).toBe('Fifteen - Love')
+
+})
+
+test('Echo "Love - Fifteen" when player button[B] get first score',() => {
+  // Arrange
+  let app = new TennisGame()
+  app.reset()
+  app.playBGetScore();
+  // Act
+  let result = app.echo()
+
+  // Assert
+  expect(result).toBe('Love - Fifteen')
+
+})
+
+test('Echo "Thirty - Forthy" when playerA get 2 score and playerB get 3 score',() => {
+  // Arrange
+  let app = new TennisGame()
+
+  app.playAGetScore();
+  app.playAGetScore();
+  app.playBGetScore();
+  app.playBGetScore();
+  app.playBGetScore();
+  // Act
+  let result = app.echo()
+
+  // Assert
+  expect(result).toBe('Thirty - Forthy')
 
 })
